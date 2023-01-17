ec813518a245d401e17bd99ec1c92a8f94b34485
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2023-01-17 23:06:52 +0600

    initial code added

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./graphql/schema";
+import { resolvers } from "./graphql/resolvers";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: process.env.PORT ? +process.env.PORT : 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
